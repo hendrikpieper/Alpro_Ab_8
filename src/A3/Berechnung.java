a0f025b43de26de8baaace418cf1516f4365e434
hendrikpieper <55532622+hendrikpieper@users.noreply.github.com>
2021-12-10 13:22:02 +0100

    Initial commit

---
+++ src/A3/Berechnung.java
@@ -0,0 +1,14 @@
+package A3;
+
+public class Berechnung extends Daten {
+    public int hoechsterTagesWert(int tag){
+        for(int i: temperaturesOverTheWeek[tag][])
+        {
+
+        }
+
+    }
+}
+
+
+
