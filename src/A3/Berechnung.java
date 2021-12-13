76b1dd7a020746317fb9717aa9b91a274da18750
hendrikpieper <55532622+hendrikpieper@users.noreply.github.com>
2021-12-13 10:03:23 +0100

    Stand 12.12.

--- src/A3/Berechnung.java
+++ src/A3/Berechnung.java
@@ -1,14 +1,57 @@
 package A3;
 
-public class Berechnung extends Daten {
-    public int hoechsterTagesWert(int tag){
-        for(int i: temperaturesOverTheWeek[tag][])
-        {
-
+public class Berechnung {       //Funktion für den höchsten Tageswert
+     public int[][] hoechsterTagesWert(int tag, int[][] daten){
+        int[][] maximalWert = new int[2][1];
+        maximalWert[0][0] = daten[tag][0];
+        for(int i = 0; i < daten[tag].length; i++) {
+            if (daten[tag][i] > maximalWert[0][0]) {
+                maximalWert[0][0] = daten[tag][i];
+                maximalWert[1][0] = i;
+            }
+        }
+        return maximalWert;
+    }
+        //Funktion für den niedrigsten Tageswert
+        public int[][] niedrigsterTagesWert(int tag, int[][] daten){
+            int[][] minimalWert = new int[2][1];
+            minimalWert[0][0] = daten[tag][0];
+            for(int i = 0; i < daten[tag].length; i++) {
+                if (daten[tag][i] > minimalWert[0][0]) {
+                    minimalWert[0][0] = daten[tag][i];
+                    minimalWert[1][0] = i;
+                }
+            }
+            return minimalWert;
         }
 
+    public int[][] hoechsterWochenWert(int[][] daten){
+        int[][][] hoechstWert = new int[3][1][1];
+        hoechstWert[0][0][0] = daten[0][0];
+        hoechstWert[]
+        for(int i = 0; i < daten.length; i++) {
+            int[][] vergleichsWert = hoechsterTagesWert(i, daten);
+            if (vergleichsWert[0][0] > hoechstWert[0][0]) {
+                    hoechstWert[0][0] = vergleichsWert[0][0];
+                    hoechstWert[1][0] = i;
+            }
+        }
+        return hoechstWert;
+    }
+
+    public int nidrigsterWochenWert(int[][] daten){
+        int wochenMinimumWert = daten[0][0]
+        for(int i = 0; i < daten.length; i++){
+            int vergleichsWert = nidrigsterTagesWert(i, daten);
+            if(vergleichsWert < wochenMinimumWert){
+                wochenMinimumWert = vergleichsWert;
+            }
+        }
+        return wochenMinimumWert;
     }
 }
+
+
 
 
 
