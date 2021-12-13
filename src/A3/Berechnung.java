01ee6f6de95906806edb9de44ede399695292278
hendrikpieper <55532622+hendrikpieper@users.noreply.github.com>
2021-12-13 13:19:11 +0100

    13.12 13:19

--- src/A3/Berechnung.java
+++ src/A3/Berechnung.java
@@ -1,9 +1,11 @@
 package A3;
+import java.util.Arrays;
 
 public class Berechnung {       //Funktion für den höchsten Tageswert
      public int[][] hoechsterTagesWert(int tag, int[][] daten){
         int[][] maximalWert = new int[2][1];
         maximalWert[0][0] = daten[tag][0];
+        maximalWert[1][0] = 0;
         for(int i = 0; i < daten[tag].length; i++) {
             if (daten[tag][i] > maximalWert[0][0]) {
                 maximalWert[0][0] = daten[tag][i];
@@ -16,6 +18,7 @@ public class Berechnung {       //Funktion für den höchsten Tageswert
         public int[][] niedrigsterTagesWert(int tag, int[][] daten){
             int[][] minimalWert = new int[2][1];
             minimalWert[0][0] = daten[tag][0];
+            minimalWert[1][0] = 0;
             for(int i = 0; i < daten[tag].length; i++) {
                 if (daten[tag][i] > minimalWert[0][0]) {
                     minimalWert[0][0] = daten[tag][i];
@@ -26,29 +29,42 @@ public class Berechnung {       //Funktion für den höchsten Tageswert
         }
 
     public int[][] hoechsterWochenWert(int[][] daten){
-        int[][][] hoechstWert = new int[3][1][1];
-        hoechstWert[0][0][0] = daten[0][0];
-        hoechstWert[]
+        int[][] hoechstWert = new int[3][1];
+        hoechstWert[0][0] = daten[0][0];
+        hoechstWert[1][0] = 0;
+        hoechstWert[2][0] = 0;
         for(int i = 0; i < daten.length; i++) {
             int[][] vergleichsWert = hoechsterTagesWert(i, daten);
             if (vergleichsWert[0][0] > hoechstWert[0][0]) {
                     hoechstWert[0][0] = vergleichsWert[0][0];
-                    hoechstWert[1][0] = i;
+                    hoechstWert[1][0] = vergleichsWert[1][0];
+                    hoechstWert[2][0] = i;
             }
         }
         return hoechstWert;
     }
 
-    public int nidrigsterWochenWert(int[][] daten){
-        int wochenMinimumWert = daten[0][0]
-        for(int i = 0; i < daten.length; i++){
-            int vergleichsWert = nidrigsterTagesWert(i, daten);
-            if(vergleichsWert < wochenMinimumWert){
-                wochenMinimumWert = vergleichsWert;
+    public int[][] nidrigsterWochenWert(int[][] daten){
+        int[][] nidrigsterWert = new int[3][1];
+        nidrigsterWert[0][0] = daten[0][0];
+        nidrigsterWert[1][0] = 0;
+        nidrigsterWert[2][0] = 0;
+        for(int i = 0; i < daten.length; i++) {
+            int[][] vergleichsWert = hoechsterTagesWert(i, daten);
+            if (vergleichsWert[0][0] < nidrigsterWert[0][0]) {
+                nidrigsterWert[0][0] = vergleichsWert[0][0];
+                nidrigsterWert[1][0] = vergleichsWert[1][0];
+                nidrigsterWert[2][0] = i;
             }
         }
-        return wochenMinimumWert;
+        return nidrigsterWert;
     }
+
+    public void datenDruckenArrayArray(int[][] zudrucken){
+        System.out.println(Arrays.toString(zudrucken));
+    }
+
+
 }
 
 
--- src/A3/Main.java
+++ src/A3/Main.java
@@ -1,4 +1,11 @@
 package A3;
+import A3.Berechnung;
+import A3.Daten;
 
 public class Main {
+    public static void main(String[] args) {
+        Berechnung berechnung = new Berechnung();
+        Daten daten = new Daten();
+        berechnung.datenDruckenArrayArray(daten.temperaturesOverTheWeek);
+    }
 }
